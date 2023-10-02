e1efd254457c850163ca346f45fb1c52b37d530c
Julia Gomes <138537752+Juliabarbo@users.noreply.github.com>
2023-10-02 12:04:51 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+#Solicita números os usuario55
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número:"))
+
+
+
+#Realiza operações 
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+#Exibe os resultados 
+print(f"soma: {soma}")
+print(f"subtracao: {subtracao}")
+print(f"multiplicacao: {multiplicacao}")
+print(f"divisao: {divisao}") 
